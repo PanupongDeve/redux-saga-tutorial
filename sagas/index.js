36bca952aca49ea89000bd0e470f70315b0abbbb
Panupong Chamsomboon <5635512110@psu.ac.th>
2022-08-08 01:36:06 +0000

    Add Saga

---
+++ sagas/index.js
@@ -0,0 +1,26 @@
+import { put, takeEvery, all } from 'redux-saga/effects'
+
+const deley = (ms) => new Promise(res => setTimeout(res, ms))
+
+function* helloSaga() {
+    console.log('Hello Sagas!')
+}
+
+function* incrementAsync() {
+    console.log('run2')
+    yield deley(1000)
+    yield put({ type: 'INCREMENT' })
+
+}
+
+function* watchIncrementAsync() {
+    console.log('run1')
+    yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+export default function* rootSaga() {
+    yield all([
+      helloSaga(),
+      watchIncrementAsync()
+    ])
+  }
